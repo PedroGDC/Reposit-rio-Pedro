02d7b585a0151c65c647264d2f8a310cb540757a
PedroGDC <127845378+PedroGDC@users.noreply.github.com>
2024-09-12 01:27:02 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const primeiroNome = "Pedro";
+const sobrenome = "Castro";
+const idade = 16;
+const altura = 1.73;
+const massaCorporal = "62";
+const cidadeNatal = "Curitiba";
+
+console.log("O meu primeiro nome é ");
+console.log(primeiroNome);
+console.log("E o meu sobre nome que carrego com o maior orgulho é");
+console.log(sobrenome);
+console.log("A minha idade atual é");
+console.log(idade);
+console.log("Essa é a minha altura des da última vez que eu me medi");
+console.log(altura);
+console.log("o minha massa corporal é");
+console.log(massaCorporal);
+console.log("A cidae que eu nasci foi");
+console.log(cidadeNatal);
